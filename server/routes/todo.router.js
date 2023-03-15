d9508b99a232a8acdf3de89525c8e4132bd86e5a
Sam Gossie <sjg3609@gmail.com>
2023-03-15 09:43:42 -0500

    Set up router paths for database connection

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -4,10 +4,68 @@ const pool = require('../modules/pool.js');
 
 // GET
 
+router.get('/', (req, res) => {
+    const queryText = `SELECT * FROM "todo";`;
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(`Error in GET ${error}`);
+        res.sendStatus(500);
+    });
+});
+
 // POST
+
+router.post('/', (req, res) => {
+    console.log('POST request made for /todo');
+    let toAdd = req.body;
+    const queryText = `INSERT INTO "todo" ("task", "complete")
+                       VALUES ($1, $2);`;
+    pool.query(queryText, [toAdd.task]).then((result) => {
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log(`Error in POST ${error}`);
+        res.sendStatus(500);
+    })
+});
 
 // PUT
 
+router.put('/completed/:id', (req, res) => {
+    let toDoID = Number(req.params.id);
+    let toDoEdit = req.params.body;
+    const queryText = `UPDATE "todo" SET "complete" = 'Uncompleted' WHERE "id" = $1;`;
+    pool.query(queryText, [toDoID]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log(`Error trying to PUT ${error}`);
+        res.sendStatus(500);
+    });
+});
+
+router.put('/uncompleted/:id', (req, res) => {
+    let toDoID = Number(req.params.id);
+    let toDoEdit = req.params.body;
+    const queryText = `UPDATE "todo" SET "complete" = 'Completed' WHERE "id" = $1;`;
+    pool.query(queryText, [toDoID]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log(`Error trying to PUT ${error}`);
+        res.sendStatus(500);
+    });
+});
+
 // DELETE
+
+router.delete('/:id', (req, res) => {
+    const deleteId = Number(req.params.id);
+    const queryText = `DELETE FROM "todo" WHERE "id" = $1;`;
+    pool.query(queryText, [deleteId]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log(`Error in DELETE ${error}`);
+        res.sendStatus(500);
+    });
+});
 
 module.exports = router;
